80b51ec1f628e3ef8eee6a5afe33afd1eb776cdd
Bárbara Oliveira <barbaragdeoliveira@hotmail.com>
2022-07-11 23:46:39 -0300

    first commit

---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,14 @@
+import Header from "../../components/Header/Header"
+import image from "../../assets/teamWork.svg"
+
+function Portfolio() {
+    return (
+        <Header
+            image={image}
+            description="ilustração de projetos entre duas mulheres de um mesmo time"
+        >
+            Portfólio:
+        </Header>
+    )
+}
+export default Portfolio
---
+++ src/pages/Sobre/Sobre.jsx
@@ -0,0 +1,14 @@
+import Header from "../../components/Header/Header"
+import image from "../../assets/woman.svg"
+
+function Sobre() {
+    return (
+        <Header
+            image={image}
+            description="ilustração de uma mulher"
+        >
+            Sobre:
+        </Header>
+    )
+}
+export default Sobre
